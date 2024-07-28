acb8b102608099748e5d0c2e909cb1b0cafb8673
ahmadasnawi <ahmadaries899@gmail.com>
2024-07-28 12:19:15 +0700

    first commit

---
+++ tugas.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section>
+        <nav>
+            <div class="left">
+                <h1>BIONOVA</h1>
+            </div>
+            <div class="mid">
+                <ul>
+                    <li>About</li>
+                    <li>Offering <i class="fa-solid fa-chevron-down"></i></li>
+                    <li>Pricing</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            <div class="right">
+                <a href="#">Log in</a>
+                <button>Request a call</button>
+            </div>
+        </nav>
+        <main>
+            <div class="kiri">
+                <div class="title">
+                    <div class="satu">
+                        <div></div>
+                        <h1>World-class</h1>
+                    </div>
+                    <div class="dua">
+                        <h1>consultants that</h1>
+                    </div>
+                    <div class="tiga">
+                        <h1>empower</h1>
+                        <button><i class="fa-solid fa-play"></i> How do we work</button>
+                    </div>
+                    <div class="empat">
+                        <h1>biotech leaders</h1>
+                    </div>
+                </div>
+                <div class="tombol">
+                    <button>contact us <i class="fa-solid fa-square-arrow-up-right"></i></button>
+                    <a href="#">request a call</a>
+                </div>
+                <div class="text">
+                    <p>Bionava collaborates with professional-led organisations to foster <br> the creation of innovative life sciences businesses</p>
+                    <div class="icon">
+                        <img src="./img/headway.png" alt="">
+                        <img src="./img/bright.png" alt="">
+                        <img src="./" alt="">
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="kanan"></div>
+        </main>
+    </section>
+</body>
+</html>
