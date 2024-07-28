d5445841b3a6f4cc700d7250099ddf5f314bf4de
ahmadasnawi <ahmadaries899@gmail.com>
2024-07-28 15:01:08 +0700

    aploud

--- tugas.php
+++ tugas.php
@@ -28,37 +28,78 @@
         </nav>
         <main>
             <div class="kiri">
-                <div class="title">
-                    <div class="satu">
-                        <div></div>
-                        <h1>World-class</h1>
+                    <div class="title">
+                        <div class="satu">
+                            <div></div>
+                            <h1>World-class</h1>
+                        </div>
+                        <div class="dua">
+                            <h1>consultants that</h1>
+                        </div>
+                        <div class="tiga">
+                            <h1>empower</h1>
+                            <button><i class="fa-solid fa-play"></i> How do we work</button>
+                        </div>
+                        <div class="empat">
+                            <h1>biotech leaders</h1>
+                        </div>
                     </div>
-                    <div class="dua">
-                        <h1>consultants that</h1>
+                    <div class="tombol">
+                        <button>contact us <i class="fa-solid fa-square-arrow-up-right"></i></button>
+                        <a href="#">request a call</a>
                     </div>
-                    <div class="tiga">
-                        <h1>empower</h1>
-                        <button><i class="fa-solid fa-play"></i> How do we work</button>
-                    </div>
-                    <div class="empat">
-                        <h1>biotech leaders</h1>
-                    </div>
-                </div>
-                <div class="tombol">
-                    <button>contact us <i class="fa-solid fa-square-arrow-up-right"></i></button>
-                    <a href="#">request a call</a>
-                </div>
-                <div class="text">
-                    <p>Bionava collaborates with professional-led organisations to foster <br> the creation of innovative life sciences businesses</p>
-                    <div class="icon">
-                        <img src="./img/headway.png" alt="">
-                        <img src="./img/bright.png" alt="">
-                        <img src="./" alt="">
-                        <img src="" alt="">
+                    <div class="text">
+                        <p>Bionava collaborates with professional-led organisations to foster <br> the creation of innovative life sciences businesses</p>
+                        <div class="icon">
+                            <img src="./img/headway.png" alt="">
+                            <img src="./img/bright.png" alt="">
+                            <img src="./img/hazel.png" alt="">
+                            <img src="./img/gstc.png" alt="">
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="kanan"></div>
+            <div class="kanan">
+                <div class="top">
+                    <div class="ungu">
+                        <div class="deskripsi">
+                            <div class="atas">
+                                <p>If you're ready to build <br> your bioventure, let's get <br> in touch</p>
+                            </div>
+                            <div class="bawah">
+                                <p>Our goal is to promote the <br> creation of innovative <br> enterprises in the life sciences.</p>
+                                <button><i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="line left">
+                        <div class="isi">
+                            <div class="tombol-atas">
+                                <button>locations</button>
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                            <h3>United bio- <br> entrepreneurs</h3>
+                            <p>Bionova unites bio- <br> entrepreneurs globally through <br> its worldwide chapters</p>
+                        </div>
+                    </div>
+                    <div class="line right">
+                        <div class="isi">
+                            <div class="tombol-atas">
+                                <button>scientist</button>
+                                <div class="sirkel">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                            <h1>34</h1>
+                            <p>Our advisors include visionary <br> scientist, pioneering <br> investors, and iconic founders.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     </section>
 </body>
